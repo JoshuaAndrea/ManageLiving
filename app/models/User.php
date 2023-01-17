5c36e6637f8b9926e630f4b80c49b82a25440f28
Joshua Andrea <90309312+Josh684077@users.noreply.github.com>
2023-01-17 23:14:24 +0100

    postcode checker

--- app/models/User.php
+++ app/models/User.php
@@ -1,11 +1,13 @@
 <?php
+require_once __DIR__ . '/../models/UserType.php';
 
-class User 
+class User
 {
-    public $id;
-    public $firstName;
-    public $lastName;
-    public $userType;
-    public $salt;
-    public $hash;
+    private int $id;
+    private string $username;
+    private string $firstName;
+    private string $lastName;
+    private UserType $userType;
+    private string $salt;
+    private string $hash;
 }
